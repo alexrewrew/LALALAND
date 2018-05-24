012d7736e46c1270e369347008dd591b9901f263
alexrewrew <alexrewrew@live.com>
2018-05-24 11:44:16 +0300

    Update 24.05

--- dev/scripts/dev.js
+++ dev/scripts/dev.js
@@ -17,12 +17,15 @@
 
         linkSmooth: $(".smooth"),
 
-        cameraTooltipClose: $('.nav-widgets__camera__tooltip_close'),
+        cameraTooltipClose: $('#close-tooltip'),
         cameraTooltip: $('.nav-widgets__camera__tooltip'),
 
         sliderDiscount: $('.slider-discount'),
 
         sliderManager: $('.slider-manager'),
+
+        cookieClose : $('#close-cookie'),
+        cookie : $('.cookie'),
 
         angleLeft: '<div class="arrow left"><svg width="16px" height="25px" viewBox="0 0 16 25" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Головна" transform="translate(-575.000000, -638.000000)" fill="#064778" stroke="#064778"> <g id="Group-28" transform="translate(256.000000, 516.000000)"> <g id="Акция-3"> <path d="M323.797542,134.05049 L334,144.252947 L332.019182,146.233765 L320,134.214583 L320.155108,134.059474 L320.114817,134.019182 L332.133999,122 L333.991015,123.857017 L323.797542,134.05049 Z" id="Combined-Shape-Copy"></path> </g> </g> </g> </g></svg></div>',
         angleRight: '<div class="arrow right"><svg width="16px" height="25px" viewBox="0 0 16 25" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Головна" transform="translate(-620.000000, -638.000000)" fill="#064778" stroke="#064778"> <g id="Group-28" transform="translate(256.000000, 516.000000)"> <g id="Акция-3"> <path d="M378.844892,134.059474 L379,134.214583 L366.980818,146.233765 L365,144.252947 L375.202458,134.05049 L365.008985,123.857017 L366.866001,122 L378.885183,134.019182 L378.844892,134.059474 Z" id="Combined-Shape-Copy-2"></path> </g> </g> </g> </g></svg></div>',
@@ -87,7 +90,11 @@
 
         // CAMERA
         DOMs.cameraTooltipClose.click(function () {
-            DOMs.cameraTooltip.hide();
+            DOMs.cameraTooltip.fadeOut('fast');
+        });
+
+        DOMs.cookieClose.click(function () {
+            DOMs.cookie.fadeOut('fast');
         });
 
 
